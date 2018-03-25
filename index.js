2bc294884d65b8d5df46ed2659d7932feb350437
John Rodriguez <jcr2125@gmail.com>
2018-03-25 23:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var newObj = {itemName: item, itemPrice: };
+ var newObj = {itemName: item, itemPrice: Math.floor(Math.random() * 100)};
 }
 
 function viewCart() {
