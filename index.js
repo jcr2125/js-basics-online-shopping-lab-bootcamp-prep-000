292b8879d471554587eef04ef8f6cd278e7d475f
John Rodriguez <jcr2125@gmail.com>
2018-03-25 23:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,9 +42,11 @@ function total() {
 function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if(cart[i].itemName === item){
-      return cart.splice(i, 1)
+      return cart.splice(i, 1);
     }
   }
+  
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
