0fa8eef4b880bb7d73ebc8a76c591f9cd5799453
John Rodriguez <jcr2125@gmail.com>
2018-03-25 23:52:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,13 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  const total = 0;
+  
+  for(let i = 0; i < cart.length; i++){
+    total += cart[i].itemPrice;
+  }
+  
+  return total;
 }
 
 function removeFromCart(item) {
